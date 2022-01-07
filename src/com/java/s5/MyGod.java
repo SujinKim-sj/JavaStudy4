aeb306344d7edec543fdf0845db99678bd93fc22
SujinKim-sj <sjsjkim0@gmail.com>
2022-01-07 09:33:04 +0900

    MyGod

---
+++ src/com/java/s5/MyGod.java
@@ -0,0 +1,17 @@
+package com.java.s5;
+
+public class MyGod {
+	private static MyGod myGod;
+	
+	private MyGod() {
+		
+	}
+	
+	public static MyGod makeGod() {
+		if(MyGod.myGod == null) {
+			MyGod.myGod = new MyGod();
+		}
+		
+		return MyGod.myGod;
+	}
+}
---
+++ src/com/java/s5/MyGodMain.java
@@ -0,0 +1,18 @@
+package com.java.s5;
+
+public class MyGodMain {
+
+	public static void main(String[] args) {
+		
+		MyGod myGod = MyGod.makeGod();
+		
+		System.out.println(myGod);
+		//SingleTon
+		
+		MyGod my2 = MyGod.makeGod();
+		System.out.println(my2);
+		System.out.println(MyGod.makeGod());
+		
+	}
+
+}
