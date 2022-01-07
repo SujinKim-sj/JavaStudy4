07b517a9a436412a3b32b721481f1a4cdbafcaab
SujinKim-sj <sjsjkim0@gmail.com>
2022-01-07 09:32:51 +0900

    Square

---
+++ src/com/java/s4/Square.java
@@ -0,0 +1,32 @@
+package com.java.s4;
+
+public class Square {
+
+	public static final int SERO = 1;
+	final int GARO_SIZE;
+	
+	public Square() {
+		this.GARO_SIZE = 5;
+	}
+	
+	static {
+		//Square.SERO = 3;
+	}
+	
+	public static void info() {
+		System.out.println("Class Method");
+		//System.out.println(this.garo);
+		System.out.println(Square.SERO);
+		//this.info2();
+	}
+	
+	public void info2() {
+		final int NUM = 1;
+		//NUM = 2;
+		System.out.println("Instance Method");
+		//System.out.println(this.garo);
+		System.out.println(Square.SERO);
+		Square.info();
+	}
+	
+}
---
+++ src/com/java/s4/SquareMain.java
@@ -0,0 +1,12 @@
+package com.java.s4;
+
+public class SquareMain {
+
+	public static void main(String[] args) {
+		int num = 10;
+		
+		//Square.sero = 10;
+		Square.info();
+	}
+
+}
